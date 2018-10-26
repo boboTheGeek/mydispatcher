68e25c7d5924902bbb02be42055871aa0443b32d
Rob Miles <rob.miles@volvo.com>
2018-10-26 15:24:36 -0400

    Initial setup for FCFS algorithm

--- algFCFS.c
+++ algFCFS.c
@@ -10,6 +10,23 @@ File Updated: Oct 15, 2018
 #include "mydispatcher.h"
 
 int firstComeFirstServe(){
+	
+	unsigned short int index = 0;
+	
+	while (processes[index].pid){
+		if (processes[index].arrivalTime == globalTimeTicker){
+			struct Process *ptr = &processes[index];
+			queue[queuePos] = ptr;
+			queue[queuePos]->exeStartTime = globalTimeTicker;
+			printf("%d\n", processes[index].remExeTime);
+			queue[queuePos]->remExeTime--;
+			printf("process_%d; at_%d; et_%d\n", processes[index].pid, processes[index].remExeTime, processes[index].exeStartTime);
+			queuePos++;
+		}
+		index++;
+	}
+
+
 	printf("fcfs\n");
     return 0;                 //return successfully
 }
