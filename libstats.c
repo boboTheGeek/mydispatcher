8dfa3c837704bde339ad5921244beb85c405127c
Robert Miles <miles.bobby@gmail.com>
2018-11-12 20:33:07 -0500

    minor changeups

--- libstats.c
+++ libstats.c
@@ -4,36 +4,33 @@ Project:      2 mydispatcher
 Programmer:   Rob Miles
 Professor:    Dr. Lee
 File Created: Nov 7, 2018
-File Updated: Nov 7, 2018
+File Updated: Nov 12, 2018
 */
 
 #include "mydispatcher.h"
 
 void processStatistics(struct Process *node){
-    FILE * fp;
-    /* open the file for writing*/
-    fp = fopen("output.dat", "w");
+    FILE * fp;                                         //create file for output.dat
+    FILE * fpsup;                                      //create file for supplimental data for documentation
     
-    float avgturnaround = 0;
+    fp = fopen("output.dat", "w");                   // open the file for writing
+    fpsup = fopen("processes.csv", "w");             // open the file for writing
+    
+    float avgturnaround = 0;                           //init some variables
 	float avgnormalizedTurnaround = 0;
 	float avgwaiting = 0;
-	
-    while (node != NULL)
-    {
-        /*
-        printf("P %d   ", node->pid);
-        printf("A %d   ", node->arrivalTime);
-        printf("S %d   ", node->exeTime);
-        printf("R %d   ", node->remExeTime);
-        printf("C %d   ", node->complete);
-        printf("D %d\n ", node->exeDoneTime);
-        */
-		node->turnaroundTime = node->exeDoneTime - node->arrivalTime;
-		node->waitTime = node->turnaroundTime - node->exeTime;
-		avgturnaround += node->turnaroundTime;
-		avgwaiting += node->waitTime;
-		avgnormalizedTurnaround += (float)(node->turnaroundTime / node->exeTime);
+                                                       //prepare headings for supplimental output file
+    fprintf(fpsup, "pid, arrival, service, remaining, start, finish, turnaround, wait, normturnaround, complete\n");
+    
+    while (node != NULL){                             //loop through all elements of the process list
+
+        node->turnaroundTime = node->exeDoneTime - node->arrivalTime;  //calculate turnaround time
+        node->waitTime = node->turnaroundTime - node->exeTime;         //calculate wait time
+        avgturnaround += node->turnaroundTime;         //add to tally for average turnaround
+        avgwaiting += node->waitTime;                  //add to tally for average wait time
+        avgnormalizedTurnaround += (float)(node->turnaroundTime / (float)node->exeTime); //add to tally for normalized turnaround time
         
+                                                       //print items to output file
         fprintf(fp, "%5d runs %d-%d: A=%d, S=%d, W=%d, F=%d, T=%d\n", \
                 node->pid, \
                 node->arrivalTime, \
@@ -43,16 +40,28 @@ void processStatistics(struct Process *node){
                 node->waitTime, \
                 node->exeDoneTime, \
                 node->turnaroundTime);
+        fprintf(fpsup, "%d, %d, %d, %d, %d, %d, %d, %d, %0.2f, %d\n", \
+                node->pid, \
+                node->arrivalTime, \
+                node->exeTime, \
+                node->remExeTime, \
+                node->exeStartTime, \
+                node->exeDoneTime, \
+                node->turnaroundTime, \
+                node->waitTime, \
+                (float)(node->turnaroundTime / (float)node->exeTime), \
+                node->complete);
         
-        node = node->next;
+        node = node->next;                             //switch to next node for while loop
 
 	}
-	avgturnaround = (float)avgturnaround / TOTAL_ROWS;
-	avgwaiting = (float)avgwaiting / TOTAL_ROWS;
-	avgnormalizedTurnaround = (float) avgnormalizedTurnaround / TOTAL_ROWS;
-	printf("\n\nAverage turnaround time = %0.4f \n", avgturnaround);
+	avgturnaround = (float)avgturnaround / (float)TOTAL_ROWS; //complete calcuation for average turnaround
+	avgwaiting = (float)avgwaiting / (float)TOTAL_ROWS;       //complete calcuation for average waiting
+	avgnormalizedTurnaround = (float) avgnormalizedTurnaround / (float)TOTAL_ROWS;       //complete calcuation for average normalized turnaround
+	printf("\n\nAverage turnaround time = %0.4f \n", avgturnaround);          //print the averages for the user
 	printf("Average normalized turnaround time = %0.4f \n", avgnormalizedTurnaround);
 	printf("Average waiting time = %0.4f \n", avgwaiting);
     
-    fclose(fp);
+    fclose(fp);                                        //close the files that we were writing to
+    fclose(fpsup);
 }
--- mydispatcher.c
+++ mydispatcher.c
@@ -32,7 +32,7 @@ void append(struct Process** head_ref, int arrivalTime_in, int exeTime_in, int p
     new_node->waitTime = 0;                            //init to no wait
     new_node->complete = 0;                            //init to incomplete
     new_node->next = NULL;                             //make this the last link
-    new_node->Qnext = NULL;                             //make this the last link
+    new_node->Qnext = NULL;                            //make this the last link
 
 
     if (*head_ref == NULL){                            //If the Linked List is empty,
@@ -47,48 +47,41 @@ void append(struct Process** head_ref, int arrivalTime_in, int exeTime_in, int p
     return;
 }
 
-
 int main(int argc, char *argv[]){
 	FILE *fp;                                          //pointer for a file
-	int arrivalTime_in, exeTime_in, pid = 1;           //
-	 
-    clock_t start, end;                                //setup for execution timer
-    double cpu_time_used;                             //
-    start = clock();                                   //
+	int arrivalTime_in, exeTime_in, pid;               //init vars for later
+    struct Process *inputProcesses;                    //init linked list of input processes
+    inputProcesses = NULL;                             //and set it to null for now
+    
+    clock_t start, end;                                //setup for execution timer - not part of main project
+    double cpu_time_used;                             //init timer var  - not part of main project
+    start = clock();                                   //start clock - not part of main project
 
-	if (!(fp = fopen(argv[1], "rb"))){
-		perror("sorry, .dat file import problem\n");
-		printf("provide the .dat file as first argument, should be in the same folder as executible\n");
+	if (!(fp = fopen(argv[1], "rb"))){                //open the input file for reading
+		printf("provide the .dat file as first argument, should be in the same folder as executible\n"); //complain if it didn't work
 	}
     
-    struct Process *inputProcesses;
-    inputProcesses = NULL;
     pid = 1;
     while (fscanf(fp, "%d %d", &arrivalTime_in, &exeTime_in) != EOF) { //grab col 1 and 2 from file if it's not the end
-        append(&inputProcesses, arrivalTime_in, exeTime_in, pid);
-        pid++;
+        append(&inputProcesses, arrivalTime_in, exeTime_in, pid);  //add row as process item to linked list
+        pid++;                                         //increase pid counter for next process
     }
     
 	
 	if (argc != 3){                                    //if you don't have 2 input params
 		printf("Input parameters should be input.dat and <ALGORITHM> to run.\n");
 		return 1;                                     //end unsuccessfully
-	}
-	else if (strcmp(argv[2], "FCFS") == 0){           //check for and run FCFS algorithm request
+	} else if (strcmp(argv[2], "FCFS") == 0){           //check for and run FCFS algorithm request
         firstComeFirstServe(inputProcesses);
-	}
-	else if (strcmp(argv[2], "RR") == 0){             //check for and run RR algorithm request
+	} else if (strcmp(argv[2], "RR") == 0){             //check for and run RR algorithm request
 		roundRobin(inputProcesses);
-	}
-	else if (strcmp(argv[2], "STN") == 0){            //check for and run STN algorithm request
-        shortestTimeNext(inputProcesses);
-    }
-	else if (strcmp(argv[2], "SRT") == 0){            //check for and run SRT algorithm request
+	} else if (strcmp(argv[2], "SPN") == 0){            //check for and run STN algorithm request
+        shortestProcessNext(inputProcesses);
+    } else if (strcmp(argv[2], "SRT") == 0){            //check for and run SRT algorithm request
         shortestRemainingTime(inputProcesses);
-	}
-	else{                                              //if the input parameter string wasn't found, warn and remind what is OK
+	} else {                                              //if the input parameter string wasn't found, warn and remind what is OK
 		printf("Make sure you're <ALGORITHM> parameter is one of the following [in all caps]:\n");
-		printf("FCFS, RR, STN, SRT\n");
+		printf("FCFS, RR, SPN, SRT\n");
 		return 1;                                     //end unsuccessfully
 	}
     
