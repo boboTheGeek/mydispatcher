03152637c6dac9ae9a717ec3e01af473ae545379
Robert Miles <miles.bobby@gmail.com>
2018-11-23 19:15:24 -0500

    queue adjustments for SRT

--- algSRT.c
+++ algSRT.c
@@ -16,9 +16,12 @@ int shortestRemainingTime(struct Process *inProc){
 	queueList = NULL;                                  //initialized to nothing, gets first process later
 	struct Process *activeProcess;                     //which process is "executing"
 	activeProcess = queueList;                         //will keep track of the executing process
-	struct Process *processIterator;                     //iterator for process selection
-	processIterator = NULL;                              //start with NULL, assign later
-	unsigned short sizeComp;                              //for process selection
+	struct Process *processIterator;                   //iterator for process selection
+	processIterator = NULL;                            //start with NULL, assign later
+    struct Process *nextProcess;                       //holds last process for when we need to remove a process
+    nextProcess = NULL;                                //it starts as null
+	unsigned short sizeComp;                          //for process selection
+    char flag = 0;                                     //incidcates if condtender is smaller
 
 	while (1) {                                               //check processes left that aren't finished
 		
@@ -30,7 +33,11 @@ int shortestRemainingTime(struct Process *inProc){
 					if(activeProcess){                        
 						if (ipIndex->exeTime < activeProcess->exeTime){//if new process is smaller than activeprocess
 							sizeComp = ipIndex->exeTime;      //set size comparison variable
-							activeProcess = ipIndex;          //switch to it      
+							nextProcess = ipIndex;          //switch to it
+                            flag = 1;
+                            printf("aP %d, ", activeProcess->pid);
+                            printf("testpoint\n");
+
 						}
 					}
 				
@@ -45,8 +52,14 @@ int shortestRemainingTime(struct Process *inProc){
 		if (activeProcess && !activeProcess->exeStartTime){   //if this process just began to execute
 			activeProcess->exeStartTime = globalTimeTicker;   //record the start time
 		}
-		
-		if (activeProcess && activeProcess->remExeTime == 0){ //if active process reminaing time is 0
+        //print queue for troublehsooting
+         struct Process *tmpQ = queueList;
+         while(tmpQ){
+             printf("%d-%d ", tmpQ->pid, tmpQ->remExeTime);
+         tmpQ = tmpQ->Qnext;
+         }
+         printf("__");
+        if (activeProcess && activeProcess->remExeTime == 0){ //if active process reminaing time is 0
 			if (!activeProcess->complete)                     //catch when stalled on empty queue
 				expiredCounter++;                             //increase tally for complete processes
 			activeProcess->complete = 1;                      //if so, mark as complete
@@ -67,7 +80,7 @@ int shortestRemainingTime(struct Process *inProc){
 				}
 			}
 			else {
-
+                //nextProcess = activeProcess->Qnext;
 				if (activeProcess->Qprev){                              //if there is a previous process
 					activeProcess->Qprev->Qnext = activeProcess->Qnext; //link to jump over active (expired)
 					activeProcess->Qnext->Qprev = activeProcess->Qprev; //link to jump reverse over expired
@@ -75,11 +88,13 @@ int shortestRemainingTime(struct Process *inProc){
 					activeProcess->Qprev = NULL;                        //housekeeping expired links
 				}
 				else {
+                    
 					queueList = activeProcess->Qnext;         //otherwise, drop the first process, link head to next
 					activeProcess->Qnext->Qprev = NULL;       //set Qprev to NULL aka HEAD
 				}
-				activeProcess = activeProcess->Qnext;         //move to the next process
-			}
+				//activeProcess = nextProcess;                  //move to the next process
+
+            }
 
 			sizeComp = 65535;                                 //set large number to compare shorter processes to
 			processIterator = queueList;                      //set iterator to head of queue
@@ -87,22 +102,26 @@ int shortestRemainingTime(struct Process *inProc){
 				activeProcess = NULL;
 			}
 			while (processIterator != NULL){                  //loop through queue list
-				if (processIterator->remExeTime < sizeComp){  //if process remaining is less than compare num
+				if (processIterator->remExeTime < sizeComp
+                    && processIterator->remExeTime != 0){  //if process remaining is less than compare num
 					activeProcess = processIterator;          //set active process to the iterator value
 					sizeComp = processIterator->remExeTime;   //set new comparison value
 				}
-				else{
-					sizeComp = 65535;
-				}
+
 				processIterator = processIterator->Qnext;     //set iterator to next in queue
 			}
+
 		}
+        if(flag){
+            activeProcess = nextProcess;
+            flag = 0;
+        }
 		if (activeProcess){
 			if (activeProcess->remExeTime>0){                 //keep old processes where they are timing wise
 				activeProcess->remExeTime--;                  //EXECUTE - decreast time remaining
 			}
-			//printf("time:%6ld   PID:%6d   AT:%4d  ", globalTimeTicker, activeProcess->pid, activeProcess->arrivalTime);
-			//printf("remains:%4d   size:%4d   ", activeProcess->remExeTime, activeProcess->exeTime);
+			printf("time:%6ld   PID:%6d   AT:%4d  ", globalTimeTicker, activeProcess->pid, activeProcess->arrivalTime);
+			printf("remains:%4d   size:%4d   ", activeProcess->remExeTime, activeProcess->exeTime);
 			printf("%d == %d \n", expiredCounter, TOTAL_ROWS);
 		}
 		globalTimeTicker++;                                   //there's more work to do, increment timer
